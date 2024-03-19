02be5ca8682d824d699483216565c6c5210277cd
sameer78699 <sameerdastagir5@gmail.com>
2024-03-19 14:14:52 +0530

    deploy

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
-
+import { TiMessages } from "react-icons/ti";
 import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
@@ -42,7 +42,7 @@ const NoChatSelected = () => {
       <div className="px-4 text-center sm:text-lg md:text-xl text-black font-semibold flex flex-col items-center gap-2">
         <p>Welcome 👋 {authUser.fullName} ❄</p>
         <p>Select a chat to start messaging</p>
-        
+        <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
     </div>
   );
